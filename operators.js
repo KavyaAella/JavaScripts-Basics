eb27b4726d4bfe3836f0a6377ff14862f10279d8
kavya Aella <kaella5622@gmail.com>
2020-04-08 18:38:38 -0400

    added ops

---
+++ operators.js
@@ -0,0 +1,11 @@
+let x = 5;
+let y = 10;
+console.log(x +y);
+
+console.log(7.4 - 5);
+console.log(4*3);
+console.log(11/2); // it gives 5.5 in javascript
+console.log(13%3); // it gives reminder
+
+
+console.log(2**3); //it acts a 2 power 3
