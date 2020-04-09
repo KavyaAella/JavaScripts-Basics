24be81dd0ae0c55bbb9100050f5b5f8bd511f74d
kavya Aella <kaella5622@gmail.com>
2020-04-09 16:12:16 -0400

    Added another

---
+++ loops.js
@@ -0,0 +1,19 @@
+// for loop iterates till the condition true
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+//while loop is iterated till the condition is true
+let j=10;
+while(j>=1){
+    console.log(j);
+    j--;
+}
+
+//do while is loop is executed before it checks the condition
+let k = 2;
+do{
+console.log(k);
+
+}while(k*k >= 4)
+
